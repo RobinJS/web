6e833efe59f2fbe3cb63c22356a5c13fae559d17
Petar Velinov <peter_velinov@abv.bg>
2014-12-10 17:38:54 +0200

    Battlefield added

--- My Web Projects/BattleShips/js/mainScript.js
+++ My Web Projects/BattleShips/js/mainScript.js
@@ -4,7 +4,8 @@ require(["stage", "config" ],
 	var grid = 50,
 		x = 0,
 		y = 0,
-		strikeFieldLeftOffset = 600,
+		battleFieldLeftOffset = 50,
+		strikeFieldLeftOffset = 650,
 		strikeFieldTopOffset = 250,
 		fieldWidth = 500,
 		fieldHeight = 500;
@@ -12,11 +13,35 @@ require(["stage", "config" ],
 
 	$('#mainCanvas').css('background-position', strikeFieldLeftOffset + 'px ' + strikeFieldTopOffset + 'px');
 
+	var battleFieldBg = new createjs.Bitmap("img/grid.png"),
+		fieldNumbers = [ new createjs.Bitmap("img/field_numbers.png"), new createjs.Bitmap("img/field_numbers.png") ];
+		fieldLetters = [ new createjs.Bitmap("img/field_letters.png"), new createjs.Bitmap("img/field_letters.png") ];
+
+	fieldNumbers[0].x = 50;
+	fieldNumbers[0].y = 200;
+	fieldNumbers[1].x = 650;
+	fieldNumbers[1].y = 200;
+	fieldLetters[0].x = 0;
+	fieldLetters[0].y = 250;
+	fieldLetters[1].x = 600;
+	fieldLetters[1].y = 250;
+
+	mainStage.addChild(fieldNumbers[0]);
+	mainStage.addChild(fieldNumbers[1]);
+	mainStage.addChild(fieldLetters[0]);
+	mainStage.addChild(fieldLetters[1]);
+
+	var battleField = new createjs.Container();
+	battleField.addChild(battleFieldBg);
+	battleField.x = battleFieldLeftOffset;
+	battleField.y = strikeFieldTopOffset;
+
+	mainStage.addChild(battleField);
+
 	var marker = new createjs.Shape();
-	marker.graphics.setStrokeStyle(1).beginFill('rgba(255, 255, 255, 0.5)').rect(strikeFieldLeftOffset, strikeFieldTopOffset, 50, 50);
+	marker.graphics.setStrokeStyle(1).beginFill('rgba(125, 209, 255, 0.7)').rect(strikeFieldLeftOffset, strikeFieldTopOffset, 50, 50);
 	marker.visible = false;
 	
-
 	var strikeFieldHitArea = new createjs.Shape();
 	strikeFieldHitArea.graphics.beginFill('rgba(255, 255, 255, 0.01)').rect(strikeFieldLeftOffset, strikeFieldTopOffset, fieldWidth, fieldHeight);
 
@@ -31,9 +56,6 @@ require(["stage", "config" ],
 	});
 
 	strikeFieldHitArea.on('mouseover', function(e){
-		;;;console.log(1);
-		// if ( (e.stageX <= strikeFieldLeftOffset || e.stageY <= strikeFieldTopOffset) || (e.stageX >= strikeFieldLeftOffset + fieldWidth || e.stageY >= strikeFieldTopOffset + fieldHeight ) ) return;
-		
 		marker.visible = true;
 	});
 
