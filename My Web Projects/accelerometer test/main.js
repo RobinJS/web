69d50b5fe3230cb6358b04b2bad09c89e2ca7eb9
Petar Velinov <peter_velinov@abv.bg>
2015-04-01 15:49:42 +0300

    magnet

--- My Web Projects/accelerometer test/main.js
+++ My Web Projects/accelerometer test/main.js
@@ -12,7 +12,8 @@ function preload() {
     // game.scale.compatibility.orientationFallback = 'viewport';
     game.scale.setupScale(770, 1280);
 
-    game.load.spritesheet('bomb', 'img/bombImg.png', 200, 200);
+    game.load.spritesheet('bomb', 'img/bombImg.png', 90, 128);
+    game.load.spritesheet('magnet', 'img/magnet.png', 90, 90);
     game.load.spritesheet('health', 'img/health.png', 24, 24);
     game.load.image('player', 'img/player.png');
 
@@ -40,7 +41,7 @@ var debug;
     - speed up the falling things
     - speed down the falling things
     - explode all baddies around you
-    - get all around you (magnet)
+        - get all around you (magnet)
     - extra live
     - move in all directions
     - try to reach the bonus level
@@ -51,6 +52,7 @@ var debug;
 */
 
 function create() {
+    game.stage.setBackgroundColor("#dce2e6");
     debug = game.add.text(0, 50, " ", { font: "42px Verdana", fill: "#ffffff", align: "center" });
     //  The scrolling starfield background
     // starfield = game.add.tileSprite(0, 0, 800, 600, 'starfield');
@@ -60,6 +62,7 @@ function create() {
     game.physics.enable(player, Phaser.Physics.ARCADE);
     player.body.collideWorldBounds = true;
     player.anchor.set(0.5);
+    player.body.immovable = true;
     
 
     //  The baddies!
@@ -68,20 +71,15 @@ function create() {
     bombs.physicsBodyType = Phaser.Physics.ARCADE;
     bombs.createMultiple(20, 'bomb');
 
-    bombs.setAll('anchor.x', 0.5);
-    bombs.setAll('anchor.y', 0.5);
-    bombs.setAll('outOfBoundsKill', true);
-    bombs.setAll('checkWorldBounds', true);
+    magnets = game.add.group();
+    magnets.enableBody = true;
+    magnets.physicsBodyType = Phaser.Physics.ARCADE;
+    magnets.createMultiple(20, 'magnet');
 
     healths = game.add.group();
     healths.enableBody = true;
     healths.physicsBodyType = Phaser.Physics.ARCADE;
     healths.createMultiple(20, 'health');
-
-    healths.setAll('anchor.x', 0.5);
-    healths.setAll('anchor.y', 0.5);
-    healths.setAll('outOfBoundsKill', true);
-    healths.setAll('checkWorldBounds', true);
 
     createEnemies();
 
@@ -116,6 +114,8 @@ function create() {
     creationTimer.loop(1000, createFallingObjects, this);
     creationTimer.start();
 
+    cursors = game.input.keyboard.createCursorKeys();
+
 }
 
 function createFallingObjects () {
@@ -124,14 +124,43 @@ function createFallingObjects () {
     bomb.scale.y = 0.5;
     var x = Math.floor(Math.random() * game.world.width - 24 ) + 12;
     bomb.reset( x, -5);
-    game.physics.arcade.moveToXY(bomb, x, game.world.height + 50, 60, 3000);
+    game.physics.arcade.moveToXY(bomb, x, game.world.height + 50, 60, 6000);
     bomb.type = 'bomb';
+    bomb.body.width = 45;
+    bomb.body.height = 70;
+
+    bomb.anchor.x = 0.5;
+    bomb.anchor.y = 0.5;
+
+    bomb.outOfBoundsKill = true;
+    bomb.checkWorldBounds = true;
+
+    var magnet = magnets.getFirstDead(false);
+    magnet.scale.x = 0.5;
+    magnet.scale.y = 0.5;
+    var x = Math.floor(Math.random() * game.world.width - 24 ) + 12;
+    magnet.reset( x, -5);
+    game.physics.arcade.moveToXY(magnet, x, game.world.height + 50, 60, 6000);
+    magnet.type = 'magnet';
+    magnet.body.width = 45;
+    magnet.body.height = 70;
+
+    magnet.anchor.x = 0.5;
+    magnet.anchor.y = 0.5;
+
+    magnet.outOfBoundsKill = true;
+    magnet.checkWorldBounds = true;
+
+
 
     var health = healths.getFirstDead(false);
     var x = Math.floor(Math.random() * game.world.width - 24 ) + 12;
     health.reset( x, -5);
-    health.rotation = game.physics.arcade.moveToXY(health, x, game.world.height + 50, 60, 3000);
+    game.physics.arcade.moveToXY(health, x, game.world.height + 50, 60, 6000);
+
     health.type = 'health';
+    health.outOfBoundsKill = true;
+    health.checkWorldBounds = true;
 }
 
 function createEnemies () {
@@ -172,14 +201,11 @@ function update() {
         //  Reset the player, then check for movement keys
         // player.body.velocity.setTo(0, 0);
 
-        // if (cursors.left.isDown)
-        // {
-        //     player.body.velocity.x = -200;
-        // }
-        // else if (cursors.right.isDown)
-        // {
-        //     player.body.velocity.x = 200;
-        // }
+        if (cursors.left.isDown) {
+            player.body.x -= 10;
+        } else if (cursors.right.isDown) {
+            player.body.x += 10;
+        }
 
         // //  Firing?
         // if (fireButton.isDown)
@@ -193,10 +219,18 @@ function update() {
         // }
 
         //  Run collision
-        game.physics.arcade.overlap(player, bombs, collisionHandler, null, this);
-        game.physics.arcade.overlap(player, healths, collisionHandler, null, this);
+        // game.physics.arcade.overlap(player, bombs, collisionHandler, null, this);
+        // game.physics.arcade.overlap(player, healths, collisionHandler, null, this);
         // game.physics.arcade.overlap(enemyBullets, player, enemyHitsPlayer, null, this);
+
+        game.physics.arcade.collide(player, [bombs, magnets, healths], collisionHandler, null, this);
     // }
+
+    healths.forEach(function(h){
+        if ( h.y > game.world.height - 150 ) {
+            game.physics.arcade.moveToObject(h, player, 400);
+        }
+    });
 
     // text.text = player.x;
     scoreText.text = "Score: " + score;
@@ -215,6 +249,16 @@ function render() {
 
     // debug.text = "speed: " + speed;
     debug.text = "step: " + step;
+
+    return;
+    game.debug.body(player);
+    bombs.children.forEach(function(bomb){
+        game.debug.body(bomb);
+    });
+
+    magnets.children.forEach(function(magnet){
+        game.debug.body(magnet);
+    });
 }
 
 
