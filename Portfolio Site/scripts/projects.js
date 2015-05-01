56f13e2e218219dd68754a91710e5c93465762d7
Petar Velinov <peter_velinov@abv.bg>
2015-05-01 23:50:34 +0300

    PF Site folder removed

--- Portfolio Site/scripts/projects.js
+++
@@ -1,6 +0,0 @@
-/* Image box-shadow efect on mouseover */
-$("#content div div a > img").mouseover(function () {
-    $(this).css('boxShadow', '0 0 6px 2px #2171C6');
-}).mouseout(function () {
-    $(this).css('boxShadow', 'none');
-});
